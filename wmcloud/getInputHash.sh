c6772e8a925bc3e78599a72b03733faa88497d12
physikerwelt <wiki@physikerwelt.de>
2025-03-22 17:45:33 +0100

    Fix script

--- wmcloud/getInputHash.sh
+++ wmcloud/getInputHash.sh
@@ -17,26 +17,24 @@ echo "File Name,Real Path,File Size (bytes),Creation Date,SHA-256"
 
 # Loop through all files in the specified directory
 for file in "$DIR"/*; do
-    # Check if it's a regular file
-    if [ -f "$file" ]; then
-        # Get realpath
-        real_path=$(realpath "$file")
+    # Get realpath
+    real_path=$(realpath "$file")
 
-        # Get file size
-        file_size=$(stat --format="%s" "$file")
+    # Get file size
+    file_size=$(stat --format="%s" "$real_path")
 
-        # Get file creation date
-        creation_date=$(stat --format="%w" "$file")
+    # Get file creation date
+    creation_date=$(stat --format="%w" "$real_path")
 
-        # If creation date is not available, fall back to last modification time
-        if [ "$creation_date" == "-" ]; then
-            creation_date=$(stat --format="%y" "$file")
-        fi
-
-        # Get SHA-256 checksum
-        sha256sum=$(sha256sum "$file" | awk '{ print $1 }')
-
-        # Output the results in CSV format
-        echo "\"$file\",\"$real_path\",$file_size,\"$creation_date\",$sha256sum"
+    # If creation date is not available, fall back to last modification time
+    if [ "$creation_date" == "-" ]; then
+        creation_date=$(stat --format="%y" "$real_path")
     fi
+
+    # Get SHA-256 checksum
+    sha256sum=$(sha256sum "$real_path" | awk '{ print $1 }')
+
+    # Output the results in CSV format
+    echo "\"$file\",\"$real_path\",$file_size,\"$creation_date\",$sha256sum"
+
 done
